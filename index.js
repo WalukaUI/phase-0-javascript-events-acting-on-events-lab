e6802b46db67fff8ee8b1807c894f994751d5e39
WalukaUI <cwaluka@yahoo.com>
2021-03-28 21:47:38 -0700

    Moving all sides

--- index.js
+++ index.js
@@ -1,1 +1,67 @@
 // Your code here
+const dodger = document.getElementById('dodger')
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+  });
+
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowUp") {
+        moveDodgerUp();
+    }
+  });
+
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowDown") {
+        moveDodgerDown();
+    }
+  });
+
+
+  function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left >= 0) {
+      dodger.style.left = `${left - 5}px`;
+    }
+  }
+
+  function moveDodgerRight(){
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left >= 0) {
+      dodger.style.left = `${left + 5}px`;
+    }else{
+        dodger.style.left = "0px";
+    }
+  }
+
+  function moveDodgerUp(){
+    const leftNumbers = dodger.style.bottom.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left >= 0) {
+        dodger.style.bottom = `${left + 5}px`;
+    }else{
+        dodger.style.bottom = "0px";
+    }
+  }
+
+  function moveDodgerDown(){
+    const leftNumbers = dodger.style.bottom.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left >= 0) {
+        dodger.style.bottom = `${left - 5}px`;
+    }
+  }
